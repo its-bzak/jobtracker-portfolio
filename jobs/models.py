f014a8243d834995ca38dedab603da4cc781951c
Ryan B <barczakryan@outlook.com>
2025-12-14 20:42:39 -0500

    Created interview model, updated unique together for application. Only one application per job per user.

--- jobs/models.py
+++ jobs/models.py
@@ -7,6 +7,11 @@ class JobPosting(models.Model):
     title = models.CharField(max_length=100)
     company_name = models.CharField(max_length=100)
     location = models.CharField(max_length=100)
+    employment_means = models.CharField(max_length=50 , choices=[
+        ('RE', 'Remote'),
+        ('ON', 'On-site'),
+        ('HY', 'Hybrid'),
+    ], default='ON')
     salary_range = models.CharField(max_length=50, blank=True, null=True)
     description = models.TextField(max_length=1000)
     posted_date = models.DateField(auto_now_add=True)
@@ -26,6 +31,7 @@ class Application(models.Model):
     job = models.ForeignKey(JobPosting, on_delete=models.CASCADE) # Each application is linked to a job posting
     application_date = models.DateField(auto_now_add=True)
     status = models.CharField(max_length=50, choices=[
+        ('DR', 'Draft'),
         ('AP', 'Applied'),
         ('IN', 'Interview'),
         ('OF', 'Offer'),
@@ -34,3 +40,20 @@ class Application(models.Model):
 
     def __str__(self):
         return f"{self.job.title} at {self.job.company_name} - {self.applicant.username}"
+    
+    class Meta:
+        unique_together = ('applicant', 'job')  # Prevent duplicate applications for the same job by the same user
+
+class Interview(models.Model):
+    application = models.ForeignKey(Application, on_delete=models.CASCADE)
+    interview_date = models.DateTimeField()
+    interviewer = models.CharField(max_length=100)
+    notes = models.TextField(max_length=1000, blank=True, null=True)
+    means_of_interview = models.CharField(max_length=50, choices=[
+        ('PH', 'Phone'),
+        ('VI', 'Video Call'),
+        ('IN', 'In-Person'),
+    ], default='PH')
+
+    def __str__(self):
+        return f"Interview for {self.application.job.title} with {self.interviewer} on {self.interview_date}"
--- jobs/serializers.py
+++ jobs/serializers.py
@@ -2,10 +2,10 @@ from rest_framework import serializers
 from .models import JobPosting, Application
 
 class JobPostingSerializer(serializers.ModelSerializer):
-    
+
     class Meta:
         model = JobPosting
-        fields = ['id', 'title', 'company_name', 'location', 'description', 'posted_date', 'employment_type']
+        fields = ['id', 'title', 'company_name', 'location', 'salary_range', 'description', 'posted_date', 'employment_type']
 
 class ApplicationSerializer(serializers.ModelSerializer):
 
