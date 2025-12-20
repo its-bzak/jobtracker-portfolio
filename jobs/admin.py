20cb5e2d145c98fba38f5acabb27379947b12091
Ryan B <barczakryan@outlook.com>
2025-12-20 11:35:08 -0500

    deleted test resume, made migrations, created answer and question models.

--- jobs/admin.py
+++ jobs/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Profile, Company, JobPosting, Application, Interview
+from .models import Profile, Company, JobPosting, Application, Interview, JobAppAnswer, JobAppQuestion
 # Register your models here.
 
 admin.site.register(JobPosting)
@@ -7,9 +7,11 @@ admin.site.register(Application)
 admin.site.register(Interview)
 #admin.site.register(Profile)
 admin.site.register(Company)
+admin.site.register(JobAppAnswer)
+admin.site.register(JobAppQuestion)
 
 @admin.register(Profile)
 class ProfileAdmin(admin.ModelAdmin): # Custom admin for Profile to ensure clean method is called
     def save_model(self, request, obj, form, change):
-        obj.full_clean()  # This will call the clean method on the model
+        obj.full_clean()  # This will call the clean() method on the model
         super().save_model(request, obj, form, change)
